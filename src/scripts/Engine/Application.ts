36d359ceaf989c874cdfb9938b678ff402109565
devanshu_BombayPlay <devanshu@bombayplay.com>
2023-09-26 22:17:11 +0530

    pixi app init

---
+++ src/scripts/Engine/Application.ts
@@ -0,0 +1,57 @@
+import { game } from "../main";
+
+
+/* 
+* It is class whose instance is treated as gameObject of the Game
+* It allow us to add compoenet on stage,
+* It allow to load media (All asynchronous are take care internally by PIXI).
+* It also contain the _app object.
+*/
+export class Application {
+
+    public _app: PIXI.Application;
+    public stage: PIXI.Container;
+    public loader: PIXI.Loader;
+
+    constructor() {
+        const resolution: number = window.devicePixelRatio;
+        this._app = new PIXI.Application({
+            autoStart: true,
+            autoDensity: true,
+            antialias: false,
+            resolution: resolution,
+            backgroundColor: 0xFFFFFF,
+            resizeTo: window,
+            width: 1280,
+            height: 720,
+        });
+
+
+        document.body.appendChild(this._app.view);
+        window.addEventListener("resize", () => {
+            this._app.renderer.resize(window.innerWidth, window.innerHeight);
+        });
+        this.stage = this._app.stage;
+        this.loader = this._app.loader;
+
+        this.init();
+
+    }
+
+    getFPS(): number {
+        return this._app.ticker.FPS;
+    }
+
+    getStageRefrence(): PIXI.Container {
+        return this.stage;
+    }
+
+    getLoaderRefrence(): PIXI.Loader {
+        return this.loader
+
+    }
+
+    public init(): void {
+        console.log("Game Init");
+    };
+}
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello");
+/* 
+* This is the entry point of the application where we create Game Object of `Application` 
+* and assigned it to window so that we can use it the game the anywhere in the game
+* Then we creating an instance of  Loader Class which is reponsible for loading the media.
+*/
+
+
+import * as PIXI from "pixi.js";
+declare global {
+    interface Window { PIXI: any; }
+    interface Window { game: Application; }
+
+}
+
+window.PIXI = PIXI;
+
+import { Application } from "./Engine/Application";
+
+// @ts-ignore               
+export  const game: any = new Application();
+// @ts-ignore               
+globalThis.__PIXI_APP__ = game;
+
+window.game = game;
